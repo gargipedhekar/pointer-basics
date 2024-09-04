3ba513350609603ba8b7010eb01e7f542eaa8127
gargipedhekar <gargi27.pedhekar@gmail.com>
2024-09-04 14:50:19 +0530

    Create pointer_addresses.cpp

---
+++ pointer_addresses.cpp
@@ -0,0 +1,21 @@
+//Name: Gargi
+//PRN:23070123052
+//Aim: To print the address of the variable usng pointers.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int *aptr = &a;
+    cout << *aptr << endl;
+    cout << aptr << endl;
+    cout << &a << endl;
+
+    return 0;
+}
+/*
+Output
+10
+0x7ffccf44bba4
+0x7ffccf44bba4
+*/
